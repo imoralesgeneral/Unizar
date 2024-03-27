f369a86d2439023a6de6a54a11a377ec052149dd
Isaac Morales General <33809673+imoralesgeneral@users.noreply.github.com>
2024-03-27 15:57:14 +0100

    Codigo cifrar2

--- crear_ficheros.ps1
+++ crear_ficheros.ps1
@@ -1,5 +1,5 @@
-# Itero desde 1 a 100
-1..100 | ForEach-Object {
+# Itero desde 1 a 1000
+1..1000 | ForEach-Object {
 	# Construyo nombre y contenido de los ficheros
 	$fileName = "$($_).txt"
 	$content = "Test $_"
